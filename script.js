309e0f97960d2eeb976cd8e5c777e93e7bab6a42
Jawed-akhtar1 <ja0935642@gmail.com>
2024-08-15 00:41:37 +0530

    commit messagee

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('playAnthemBtn').addEventListener('click', function() {
+    let audio = new Audio('jaarejahanseachha.mp3'); // Replace with an actual national anthem URL or file
+    audio.play();
+});
